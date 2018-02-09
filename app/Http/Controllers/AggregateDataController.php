fa51efbecf19ab804d2c3f3b0c134a260b309b3c
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2018-02-09 13:02:01 -0800

    Added function that returns average income for 3 basic paths

--- app/Http/Controllers/AggregateDataController.php
+++ app/Http/Controllers/AggregateDataController.php
@@ -3,8 +3,43 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\MajorPath;
+
 
 class AggregateDataController extends Controller
 {
-    //
+    public function getAverageIncomeByStudentPath(){
+        $paths = MajorPath::where('student_path',1)
+            ->with('majorPathWage')
+            ->get();
+        $averages=[];
+        foreach ($paths as $path){
+            array_push($averages, $path->majorPathWage->avg_annual_wage);
+        }
+        $bachelors_degree_avg = array_sum($averages)/count($averages);
+
+        $paths = MajorPath::where('student_path',2)
+            ->with('majorPathWage')
+            ->get();
+        $averages=[];
+        foreach ($paths as $path){
+            array_push($averages, $path->majorPathWage->avg_annual_wage);
+        }
+        $some_college_avg = array_sum($averages)/count($averages);
+
+        $paths = MajorPath::where('student_path',3)
+            ->with('majorPathWage')
+            ->get();
+        $averages=[];
+        foreach ($paths as $path){
+            array_push($averages, $path->majorPathWage->avg_annual_wage);
+        }
+        $master_avg = array_sum($averages)/count($averages);
+        $averages = [
+            'some_college_avg' => $some_college_avg,
+            'bachelors_avg' => $bachelors_degree_avg,
+            'masters_avg' => $master_avg
+        ];
+        return ($averages);
+    }
 }
