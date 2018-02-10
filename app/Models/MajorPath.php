372c6dfe53b01d6668881bac0339624d095b10e0
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2018-02-09 16:41:34 -0800

    Added unit test, also altered migration to no longer include timestamps on MajorPath

--- app/Models/MajorPath.php
+++ app/Models/MajorPath.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class MajorPath extends Model
 {
+    public $timestamps = false;
     public $primaryKey = 'id';
     protected $fillable = [
         'student_path',
