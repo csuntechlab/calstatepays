962de1ec8940162035db6685a3c51875e3c26ca7
Alejandro Martinez <hello@alejandrom.xyz>
2018-02-11 23:56:35 -0800

    Made changes to Controller Function logic to appropriatly handle the case in which multiple paths are avaliable for a given industry. Will now collect all averages and compute a larger average. Unit Test updateded to fit new changes.

--- app/Http/Controllers/AggregateDataController.php
+++ app/Http/Controllers/AggregateDataController.php
@@ -4,13 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use App\IndustryWage;
-use App\NaicsTitle;
+use App\Models\IndustryWage;
+use App\Models\NaicsTitle;
 
 class AggregateDataController extends Controller
 {
     public function getAverageIncomeByIndustry()
     {
-		return NaicsTitle::with('industryWage')->get();
+		$naicsTitles =  NaicsTitle::with('industryWage')->get()->keyBy('naics_code');
+		foreach ($naicsTitles as $industry) {
+			$industryWages = $industry->industryWage->toArray();
+			$avg_annual_wage_5 = array_column( $industryWages ,'avg_annual_wage_5');
+			$avg_annual_wage_10 = array_column( $industryWages ,'avg_annual_wage_10');
+			$industry['avg_annual_wage_5'] = array_sum($avg_annual_wage_5) / count($avg_annual_wage_5);
+			$industry['avg_annual_wage_10'] = array_sum($avg_annual_wage_10) / count($avg_annual_wage_10);
+		}
+		return $naicsTitles;
     }
 }
--- tests/Feature/AggregateDataControllerTest.php
+++ tests/Feature/AggregateDataControllerTest.php
@@ -8,9 +8,9 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 use App\Http\Controllers\AggregateDataController;
-use App\NaicsTitle;
-use App\IndustryPathType;
-use App\IndustryWage;
+use App\Models\NaicsTitle;
+use App\Models\IndustryPathType;
+use App\Models\IndustryWage;
 
 class AggregateDataControllerTest extends TestCase
 {
@@ -25,37 +25,67 @@ class AggregateDataControllerTest extends TestCase
     {
     	$naics_code = rand(0,1000);
     	$naics_title = str_random(15);
-    	$avg_annual_wage_5 = rand(0,100000);
-    	$avg_annual_wage_10 = rand(50000,150000);
+    	
+        $avg_annual_wage_5_1 = rand(0,100000);
+    	$avg_annual_wage_10_1 = rand(50000,150000);
+        
+        $avg_annual_wage_5_2 = rand(0,100000);
+        $avg_annual_wage_10_2 = rand(50000,150000);
+
+        $avg_annual_wage_5_total = ($avg_annual_wage_5_1+$avg_annual_wage_5_2)/2;
+        $avg_annual_wage_10_total = ($avg_annual_wage_10_1+$avg_annual_wage_10_2)/2;
+
     	$industry_path_id = 1;
     	
     	NaicsTitle::create(
-    		['naics_code' => $naics_code,
-    		 'naics_title' => $naics_title
+    		[
+                'naics_code' => $naics_code,
+                'naics_title' => $naics_title
     		]
     	);
     	IndustryPathType::create(
-    		['id' => $industry_path_id,
-    		'entry_status' => "FTF",
-    		'naics_code' => $naics_code,
-    		'student_path' => '1',
-    		'population_sample_id' => rand(100,10000),
-    		'university_majors_id' => rand(100,1000)
+    		[
+                'id' => $industry_path_id,
+        		'entry_status' => "FTF",
+        		'naics_code' => $naics_code,
+        		'student_path' => '1',
+        		'population_sample_id' => rand(100,10000),
+        		'university_majors_id' => rand(100,1000)
     		]
     	);
+        IndustryPathType::create(
+            [
+                'id' => $industry_path_id+1,
+                'entry_status' => "TTF",
+                'naics_code' => $naics_code,
+                'student_path' => '1',
+                'population_sample_id' => rand(100,10000),
+                'university_majors_id' => rand(100,1000)
+            ]
+        );
+
         IndustryWage::create(
-        	['id' => $industry_path_id,
-        	 'avg_annual_wage_5' => $avg_annual_wage_5,
-        	 'avg_annual_wage_10' => $avg_annual_wage_10
+        	[
+                'id' => $industry_path_id,
+            	'avg_annual_wage_5' => $avg_annual_wage_5_1,
+            	'avg_annual_wage_10' => $avg_annual_wage_10_1
         	]
+        );
+        IndustryWage::create(
+            [
+                'id' => $industry_path_id+1,
+                'avg_annual_wage_5' => $avg_annual_wage_5_2,
+                'avg_annual_wage_10' => $avg_annual_wage_10_2
+            ]
         );
 
     	$data = $this->aggregateDataController->getAverageIncomeByIndustry();
-    	$content = (json_decode($data, true))[0];
-   
-   		$this->assertEquals($naics_code,$content['naics_code']);
-   		$this->assertEquals($naics_title,$content['naics_title']);
-   		$this->assertEquals($avg_annual_wage_5, $content['industry_wage'][0]['avg_annual_wage_5']);
-   		$this->assertEquals($avg_annual_wage_10, $content['industry_wage'][0]['avg_annual_wage_10']);
+        
+        $testData  = $data[$naics_code];
+
+   		$this->assertEquals($naics_code,  $testData['naics_code']);
+   		$this->assertEquals($naics_title,  $testData['naics_title']);
+   		$this->assertEquals($avg_annual_wage_5_total,  $testData['avg_annual_wage_5']);
+   		$this->assertEquals($avg_annual_wage_10_total, $testData['avg_annual_wage_10']);
     }
 }
