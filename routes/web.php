60463393ef55609369d9e7a3c9dd3fbe24e6a7f6
Joesdev <joesilvadev@gmail.com>
2018-02-26 21:49:44 +0000

    Added route for retreiving aggregate PFRE data

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Aggregate/PFRE', 'AggregateDataController@getAggregateCollegesPFRE')->name('aggregate.colleges.PFRE');
+Route::post('/Aggregate/PFRE', 'AggregateDataController@getAggregateCollegesPFRE')->name('aggregate.colleges.PFRE');
