694abdec5a63dcbded607441e4cb35a3183f1499
Joesdev <joesilvadev@gmail.com>
2018-02-26 22:20:46 +0000

    Added route for top10 majors and function with psuedo code

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/Aggregate/TopTenMajors','AggregateDataController@getAggregateTopTenMajors')->name('aggregate.top.majors');
