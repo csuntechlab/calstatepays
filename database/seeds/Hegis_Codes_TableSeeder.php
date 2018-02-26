169c405ed57c69338b75ef94686ef72fb0c8dc87
Joesdev <joesilvadev@gmail.com>
2018-02-26 05:40:39 +0000

    Cleaned up json data, removed header which was saved as data

--- database/seeds/Hegis_Codes_TableSeeder.php
+++ database/seeds/Hegis_Codes_TableSeeder.php
@@ -15,11 +15,11 @@ class Hegis_Codes_TableSeeder extends Seeder
         $json = File::get("database/data/hegis_codes.json");
         $data = json_decode($json);
         foreach($data as $row){
-            DB::table('hegis_codes')->insert([
-                'hegis_code' => $row->hegis_code,
-                'major'      => $row->major,
-                'university' => $row->university
-            ]);
+                DB::table('hegis_codes')->insert([
+                    'hegis_code' => $row->hegis_code,
+                    'major'      => $row->major,
+                    'university' => $row->university
+                ]);
         };
     }
 }
