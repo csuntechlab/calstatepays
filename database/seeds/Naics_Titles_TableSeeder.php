503528a9bf5bb9bab0052c9295c00d4ffa8e0944
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2018-02-28 13:28:36 -0800

    Added seeder for NAICS Titles

---
+++ database/seeds/Naics_Titles_TableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Naics_Titles_TableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $json = File::get("database/data/naics_titles.json");
+        $data = json_decode($json);
+        foreach($data as $row){
+            DB::table('naics_titles')->insert([
+                'naics_code'              => $row->naics_code,
+                'naics_title' => $row->naics_title
+            ]);
+        };
+    }
+}
