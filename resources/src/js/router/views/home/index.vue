6c4aaa1fa349fce46d26dfe37e2e948b026700f6
Mikkal Mcnulty <mcnultymikkal@gmail.com>
2018-02-28 13:56:01 -0800

    Create rough draft of home page structure

--- resources/src/js/router/views/home/index.vue
+++ resources/src/js/router/views/home/index.vue
@@ -1,6 +1,31 @@
 <template>
 	<div>
-		Home Page
+		<div class="section">
+			<div class="row bg">
+				<div class="col" style="height: 400px;">
+					VIDEO
+				</div>
+			</div>
+			<div class="container-fluid no-gutters">
+				<div class="row" style="height: 300px;">
+					<div class="col">
+						<h4>More Education, More Income</h4>
+						<img src="https://i.gyazo.com/cb929c9e6ed7eb5ac11531007d11f0ad.png" style="height: 100%"></img>
+					</div>
+					<div class="col">
+						<h4>More Education, More Income</h4>
+						<img src="https://i.gyazo.com/cb929c9e6ed7eb5ac11531007d11f0ad.png" style="height: 100%"></img>
+					</div>
+					<div class="col">
+						<h4>More Education, More Income</h4>
+						<img src="https://i.gyazo.com/cb929c9e6ed7eb5ac11531007d11f0ad.png" style="height: 100%"></img>
+					</div>
+				</div>
+				<div class="row" style="height: 300px;">
+					banner
+				</div>
+			</div>		
+		</div>
 	</div>
 </template>
 <script>
@@ -8,3 +33,22 @@
 
 	}
 </script>
+
+<style>
+/*	* {
+		border-style: solid;
+		border-width: 1px;
+	}*/
+	.bg { 
+	    /* The image used */
+	    background-image: url("https://i.gyazo.com/5c5d06bc31eef4555907e703c4a3baa2.jpg");
+
+	    /* Full height */
+	    height: 100%; 
+
+	    /* Center and scale the image nicely */
+	    background-position: center;
+	    background-repeat: no-repeat;
+	    background-size: cover;
+	}  
+</style>
