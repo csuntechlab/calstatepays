7de17b26f3bdebdff9537a0c710dbb51226300ae
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2018-03-20 13:56:45 -0700

    Added route, controller, and function to acccess Learn And Earn API.

---
+++ app/Http/Controllers/LearnAndEarnController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\Client;
+
+class LearnAndEarnController extends Controller
+{
+    public function getCollegeMajor($schoolId, $majorId)
+    {
+        $client = new Client(); //GuzzleHttp\Client
+        $result = $client->get('https://www.metalab.csun.edu/learnandearn/api/major-data/'. $schoolId .'/'. $majorId);
+//        $result = $client->get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
+        return $result;
+    }
+}
