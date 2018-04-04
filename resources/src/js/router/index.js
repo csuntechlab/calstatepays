d4c170829946890257e6477634f002cb71bcf734
arthurwirsching <arthur.wirsching.22@my.csun.edu>
2018-04-04 13:45:09 -0700

    Added Vuetify and FAQ Page

--- resources/src/js/router/index.js
+++ resources/src/js/router/index.js
@@ -2,12 +2,12 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import store from '../store';
 
-
 //PAGES
 import home from './views/home/index.vue';
 import pfre from './views/pfre/index.vue';
 import majors from './views/majors/index.vue';
 import industries from './views/industries/index.vue';
+import faq from './views/faq/index.vue';
 
 // INIT VUE-ROUTER
 Vue.use(VueRouter);
@@ -17,7 +17,8 @@ const router = new VueRouter({
 		{ path: '/', component: home },
 		{ path: '/majors', component: majors },
 		{ path: '/pfre', component: pfre },
-		{ path: '/industries', component: industries}
+		{ path: '/industries', component: industries},
+		{ path: '/faq', component: faq}
 	]
 });
 
