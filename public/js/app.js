07d7243cec86e6da373e4d76bfb356a5ec03c9a6
Mikkal Mcnulty <mcnultymikkal@gmail.com>
2018-04-16 14:07:46 -0700

    get rid of place holder data

--- public/js/app.js
+++ public/js/app.js
@@ -76094,49 +76094,8 @@ var fetchIndustriesAPI = function fetchIndustriesAPI(success, error) {
             maximum: .15,
             actual: 0
         }
-    },
-    pfreMockData: {
-        years: {
-            start: 2,
-            middle: 7,
-            end: 12,
-            actual: 4
-        },
-        earnings: {
-            minimum: 20000,
-            average: 50000,
-            maximum: 100000,
-            actual: 45000
-        },
-        returnOnInvestment: {
-            minimum: 0,
-            average: .075,
-            maximum: .15,
-            actual: .03
-        }
-        /*
-        pfreData: {
-            years: {
-                start: 2,
-                middle: 7,
-                end: 12,
-                actual: 4
-            },
-            earnings: {
-                minimum: 20000,
-                average: 50000,
-                maximum: 100000,
-                actual: 67000,
-            },
-            returnOnInvestment: {
-                minimum: 0,
-                average: .075,
-                maximum: .15,
-                actual: .083,
-            }
-        },
-        */
-    } });
+    }
+});
 
 /***/ }),
 /* 153 */
--- resources/src/js/store/modules/pfre/state.js
+++ resources/src/js/store/modules/pfre/state.js
@@ -23,47 +23,5 @@ export default {
             actual: 0,
         }
     },
-    pfreMockData: {
-        years: {
-            start: 2,
-            middle: 7,
-            end: 12,
-            actual: 4
-        },
-        earnings: {
-            minimum: 20000,
-            average: 50000,
-            maximum: 100000,
-            actual: 45000,
-        },
-        returnOnInvestment: {
-            minimum: 0,
-            average: .075,
-            maximum: .15,
-            actual: .03,
-        }
-    }
-    /*
-    pfreData: {
-        years: {
-            start: 2,
-            middle: 7,
-            end: 12,
-            actual: 4
-        },
-        earnings: {
-            minimum: 20000,
-            average: 50000,
-            maximum: 100000,
-            actual: 67000,
-        },
-        returnOnInvestment: {
-            minimum: 0,
-            average: .075,
-            maximum: .15,
-            actual: .083,
-        }
-    },
-    */ 
 };
 
