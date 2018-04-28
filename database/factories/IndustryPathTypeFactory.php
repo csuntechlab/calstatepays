a3e10a0f51563a87c4a588f067ea60f0c593d4a4
Mikkal Mcnulty <mcnultymikkal@gmail.com>
2018-04-27 17:55:32 -0700

    set up the factory to have parameters

--- database/factories/IndustryPathTypeFactory.php
+++ database/factories/IndustryPathTypeFactory.php
@@ -12,13 +12,13 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
-$factory->define(App\Models\IndustryPathType::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\IndustryPathType::class, function (Faker\Generator $faker, $params) {
 
     return [
         'entry_status' => "All",
         'naics_code' => $faker->numberBetween(1, 99),
         'student_path' => 4,
         'population_sample_id'=> $faker->unique()->numberBetween(1, 999),
-        'university_majors_id' => 1
+        'university_majors_id' => $params['university_majors_id']
     ];
 });
