91d9f7c0c736339d1c5fed231aadf99f49f92e0c
Joesdev <joesilvadev@gmail.com>
2018-05-04 18:01:16 +0000

    Added data file to factory

--- database/factories/NaicsTitleFactory.php
+++ database/factories/NaicsTitleFactory.php
@@ -1,13 +1,12 @@
 <?php
-
 $json = File::get("database/data/naics_titles.json");
 $data = json_decode($json, true);
 
-$factory->define(App\Models\NaicsTitle::class, function (Faker\Generator $faker) {
-    dd($faker->shuffle($data));
+$factory->define(App\Models\NaicsTitle::class, function (Faker\Generator $faker) use($data) {
+    $row = $faker->unique()->randomElement($data);
     return [
-        'naics_code' => $faker->unique()->numberBetween(10,80),
-        'naics_title' => $faker->unique()->safeEmail,
-        'image' => $password ?: $password = bcrypt('secret'),
+        'naics_code' => $row['naics_code'],
+        'naics_title' => $row->['naics_title'],
+        'image' => $row->image
     ];
 });
