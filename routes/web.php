12f76e1a05e0aad0b72c13f8c3bea23534c5447f
Joesdev <joesilvadev@gmail.com>
2018-05-09 18:21:25 +0000

    Removed routes where functions no longer exist

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,3 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/Aggregate/PFRE', 'AggregateDataController@getAggregateUniversitiesPFREGraphData')
-    ->name('aggregate.PFRE.view');
-Route::post('/Aggregate/PFRE', 'AggregateDataController@getAggregateUniversitiesPFRE')
-    ->name('aggregate.PFRE');
