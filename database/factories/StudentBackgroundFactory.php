3c82dc6b7ef3051ba24bd786802c7faa4b8248ef
Joesdev <joesilvadev@gmail.com>
2018-05-25 05:29:23 +0000

    Modified StudentBackground Factory

--- database/factories/StudentBackgroundFactory.php
+++ database/factories/StudentBackgroundFactory.php
@@ -1,13 +1,11 @@
 <?php
 
 $factory->define(App\Models\StudentBackground::class, function (Faker\Generator $faker, $param){
-    $education_array = ['FTF', 'FTT'];
-    $education_level = $faker->randomElement($education_array);
 
     return [
-        'age_range_id'        => $param['age_range_id'],
-        'education_level'     => $education_level,
-        'investment_id'       => 1,
-        'university_major_id' => 1
+        'university_major_id'  => $param['university_major_id'],
+        'age_range_id'         => $param['age_range_id'],
+        'age_range_name'       => $param['age_range_name'],
+        'education_level_name' => $param['education_level_name'],
     ];
 });
--- database/seeds/University_Majors_TableSeeder.php
+++ database/seeds/University_Majors_TableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\StudentBackground;
 
 class University_Majors_TableSeeder extends Seeder
 {
@@ -15,7 +16,13 @@ class University_Majors_TableSeeder extends Seeder
         $data = json_decode($json);
 
         //Data for Student_Background table
-        $education_level = ['FTT', 'FTF'];
+        $education_levels = ['FTT', 'FTF'];
+        $age_ranges = [
+            [ 'age_range_id' => 1,'age_range_name' => '18 - 19'],
+            [ 'age_range_id' => 2,'age_range_name' => '20 - 24'],
+            [ 'age_range_id' => 3,'age_range_name' => '24 - 26'],
+            [ 'age_range_id' => 4,'age_range_name' => '26 +']
+        ];
         foreach($data as $row){
             DB::table('university_majors')->insert([
                 'id'            => $row->id,
@@ -24,6 +31,16 @@ class University_Majors_TableSeeder extends Seeder
                 'university_id' => $row-> university_id
             ]);
 
+            foreach($age_ranges as $age_range){
+                foreach($education_levels as $education_level){
+                    factory(StudentBackground::class)->create([
+                        'university_major_id' => $row->id,
+                        'age_range_id' => $age_range['age_range_id'],
+                        'age_range_name' => $age_range['age_range_name'],
+                        'education_level_name' => $education_level
+                    ]);
+                }
+            }
 
         };
     }
