51b889124473e914b4320b150b154a0e2cec6902
Joesdev <joesilvadev@gmail.com>
2018-05-29 22:57:32 +0000

    building relationship to population in University_Majors_TableSeeder

--- database/seeds/University_Majors_TableSeeder.php
+++ database/seeds/University_Majors_TableSeeder.php
@@ -3,7 +3,12 @@
 use Illuminate\Database\Seeder;
 use App\Models\StudentBackground;
 use App\Models\Investment;
-
+use App\Models\MajorPath;
+use App\Models\UniversityMajor;
+use App\Models\MajorPathWage;
+use App\Models\Population;
+use App\Models\IndustryPathType;
+use App\Models\IndustryWage;
 class University_Majors_TableSeeder extends Seeder
 {
     /**
@@ -41,16 +46,50 @@ class University_Majors_TableSeeder extends Seeder
 
         ];
 
+        //Data for major_paths table
+        $student_paths = [
+            [ 'student_path' => 1],
+            [ 'student_path' => 2],
+            [ 'student_path' => 3]
+        ];
+        $years = [
+            ['years' => 2],
+            ['years' => 5],
+            ['years' => 10],
+        ];
+
+        //Data for_industry_path_types
+        $naics = [
+            ['code' => 21],
+            ['code' => 11],
+            ['code' => 22],
+            ['code' => 55],
+            ['code' => 48],
+            ['code' => 23],
+            ['code' => 71],
+            ['code' => 53],
+            ['code' => 72],
+            ['code' => 42],
+            ['code' => 56],
+            ['code' => 31],
+            ['code' => 21],
+            ['code' => 44],
+            ['code' => 92],
+            ['code' => 52],
+            ['code' => 54],
+            ['code' => 62],
+            ['code' => 61],
+
+        ];
+
         //Each University_Major row will have 8 related StudentBackground rows, each StudentBackground row will have 24
         //related Investment rows,so then each University_Major row has 576 possible combinations for Investment data.
         foreach($data as $row){
-            $university_major = DB::table('university_majors');
-            $university_major->insert([
-                'hegis_code'    => $row->hegis_code,
-                'college_id'    => 1,
-                'university_id' => $row-> university_id
-            ]);
-            $university_major = $university_major->first();
+            $university_major = new UniversityMajor();
+            $university_major->hegis_code = $row->hegis_code;
+            $university_major->college_id = 1;
+            $university_major->university_id = $row->university_id;
+            $university_major->save();
             foreach($age_ranges as $age_range){
                 foreach($education_levels as $education_level){
                     $student_background = factory(StudentBackground::class)->create([
@@ -69,7 +108,29 @@ class University_Majors_TableSeeder extends Seeder
                     }
                 }
             }
+            //create 3 Major Paths
+            foreach($student_paths as $student_path){
+                foreach($years as $year) {
+                    $majorPath = new MajorPath();
+                    $majorPath->student_path = $student_path['student_path'];
+                    $majorPath->university_majors_id = $university_major->id;
+                    $majorPath->entry_status = 'All';
+                    $majorPath->years  = $year['years'];
+                    $majorPath->save();
 
+                    factory(MajorPathWage::class)->create(['major_path_id'=> $majorPath->id, 'population_sample_id' => $majorPath->id]);
+                    factory(Population::class)->create(['id' => $majorPath->id])->id;
+                }
+            }
+
+            foreach($naics as $naic){
+                $population = factory(Population::class)->create()->id;
+                factory(IndustryPathType::class)->create([
+                    'naics_code'           => $naic['code'],
+                    'university_majors_id' => $university_major->id,
+                    'population_sample_id' => $population
+                ]);
+            }
         };
     }
 }
