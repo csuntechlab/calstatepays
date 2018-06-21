82258bbc5dec5d2d6b29e0c300b29713a35530f0
alecnicolas <alecnmarcum@gmail.com>
2018-06-21 13:15:39 -0700

    created unit test for majorService -- controller test still works

--- tests/Services/MajorServiceTest.php
+++ tests/Services/MajorServiceTest.php
@@ -5,17 +5,24 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use App\Models\FieldOfStudy;
 use App\Models\HEGISCode;
+use App\Models\UniversityMajor;
 use App\Services\MajorService;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class MajorServiceTest extends TestCase
 {
     use DatabaseMigrations;
+    protected $majorService;
+    
+    public function setUp(){
+        parent::setUp();
+        $this->majorService = new MajorService();
+    }
 
     public function test_getAllHegisCodes_ensure_returns_all_rows() {
-        $majorService = new MajorService();
+        
         $this->seed('Hegis_Codes_TableSeeder');
-        $response = $majorService->getAllHegisCodes();
+        $response = $this->majorService->getAllHegisCodes();
 
         $this->arrayHasKey(0, $response);
         $this->arrayHasKey("hegis_code", $response[0]);
@@ -25,12 +32,22 @@ class MajorServiceTest extends TestCase
     }
 
     public function test_getAllFieldOfStudies_ensure_returns_all_rows() {
-        $majorService = new MajorService();
         $this->seed('Field_Of_Studies_TableSeeder');
-        $response = $majorService->getAllFieldOfStudies();
+        $response = $this->majorService->getAllFieldOfStudies();
 
         $this->arrayHasKey("name", $response[0]);        
         $this->arrayHasKey("id", $response[0]);        
         $this->assertEquals(FieldOfStudy::count(), count($response));
+    }
+
+    public function test_getMajorEarnings_ensure_returns_all_major_path_wages(){
+        $this->seed('University_Majors_TableSeeder');
+        $response = $this->majorService->getMajorEarnings(22021, 1153);
+        $this->arrayHasKey("id", $response[0]);        
+        $this->arrayHasKey("student_path", $response[0]);    
+        $this->arrayHasKey("university_majors_id", $response[0]);        
+        $this->arrayHasKey("entry_status", $response[0]);
+        $this->arrayHasKey("years", $response[0]);        
+        $this->arrayHasKey("major_path_wage", $response[0]);
     }
 }
