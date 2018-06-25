5527e319be651dcec5ddea92957266abedfbb05b
Joesdev <joesilvadev@gmail.com>
2018-06-25 20:52:00 +0000

    removed timestamps from universitymajors model

--- app/Models/UniversityMajor.php
+++ app/Models/UniversityMajor.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class UniversityMajor extends Model
 {
     public $primaryKey = 'id';
+    public $timestamps = false;
 
     protected $fillable = [
         'hegis_code',
