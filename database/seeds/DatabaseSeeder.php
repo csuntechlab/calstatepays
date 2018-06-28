9e6b480f71a10b68e157c66752cf114db039ca3f
Joesdev <joesilvadev@gmail.com>
2018-06-28 22:06:57 +0000

    Removed unused json files and seeders for industry_paths and population

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
         $this->call(Universities_TableSeeder::class);
         $this->call(Naics_Titles_TableSeeder::class);
         $this->call(Student_Paths_TableSeeder::class);
-        /*$this->call(Industry_Path_Types_TableSeeder::class);*/
-        /*$this->call(Populations_TableSeeder::class);*/
-        $this->call(University_Majors_TableSeeder::class);
         $this->call(Field_Of_Studies_TableSeeder::class);
-        $this->call(Hegis_Categories_TableSeeder::class);        
+        $this->call(Hegis_Categories_TableSeeder::class);
+
+        $this->call(University_Majors_TableSeeder::class);
+        
         /*$this->call(Major_Paths_TableSeeder::class);*/
     }
 }
