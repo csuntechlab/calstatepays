8eac8ddf3bebcf6fd0626143249924e443fa8dd9
Mikkal Mcnulty <mcnultymikkal@gmail.com>
2018-07-16 16:39:43 -0700

    add the tablaeu viz template

--- resources/src/js/router/views/research/index.vue
+++ resources/src/js/router/views/research/index.vue
@@ -92,6 +92,29 @@
                 </div>
             </div>
         </main>
+        <div class="container">
+            <div class='tableauPlaceholder' id='viz1531346960828' style='position: relative'>
+                <noscript>
+                    <a href='https://www.sandbox.csun.edu/metalab/csumetrola#/'>
+                        <img alt='Labor Market Outcomes for CSUN StudentsPost Education Earnings Data and Industries of Employment After Exit ' src='https://public.tableau.com/static/images/Ca/CalStatePays-CSUNEarningsOverTimeandIndustriesofEmployment/CSUNLaborMarketOutcomes/1_rss.png' style='border: none' />
+                    </a>
+                </noscript>
+                <object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' />
+                    <param name='embed_code_version' value='3' />
+                    <param name='site_root' value='' />
+                    <param name='name' value='CalStatePays-CSUNEarningsOverTimeandIndustriesofEmployment/CSUNLaborMarketOutcomes' />
+                    <param name='tabs' value='no' />
+                    <param name='toolbar' value='yes' />
+                    <param name='static_image' value='https://public.tableau.com/static/images/Ca/CalStatePays-CSUNEarningsOverTimeandIndustriesofEmployment/CSUNLaborMarketOutcomes/1.png' />
+                    <param name='animate_transition' value='yes' />
+                    <param name='display_static_image' value='yes' />
+                    <param name='display_spinner' value='yes' />
+                    <param name='display_overlay' value='yes' />
+                    <param name='display_count' value='yes' />
+                    <param name='filter' value='publish=yes' />
+                </object>
+            </div> 
+        </div>
     </div>
 </template>
 <script>
