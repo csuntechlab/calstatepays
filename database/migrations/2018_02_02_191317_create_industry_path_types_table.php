e7bfd5e6c0fa3c3b46c425412c3c59cffecf01c0
ChanM0 <mikec201722@gmail.com>
2018-08-03 12:36:42 -0700

    Fix inustry path types migration. Exclude nullable attribute.

--- database/migrations/2018_02_02_191317_create_industry_path_types_table.php
+++ database/migrations/2018_02_02_191317_create_industry_path_types_table.php
@@ -16,7 +16,7 @@ class CreateIndustryPathTypesTable extends Migration
         Schema::create('industry_path_types', function (Blueprint $table) {
             $table->increments('id');
             $table->string('entry_status');
-            $table->integer('naics_code')->nullable();
+            $table->integer('naics_code');
             $table->integer('student_path');
             $table->integer('population_sample_id');
             $table->integer('university_majors_id');
