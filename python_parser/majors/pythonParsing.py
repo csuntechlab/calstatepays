9870bc9a1bd0d03528899b2984fd309ebf615b04
carlosbenavides123 <carlos7legend@gmail.com>
2018-08-06 18:06:57 -0700

    python parse better pull this

---
+++ python_parser/majors/pythonParsing.py
@@ -0,0 +1,47 @@
+
+# coding: utf-8
+
+# In[11]:
+
+
+import pandas as pd
+import numpy as np
+df = pd.read_csv('master_major.csv')
+
+
+# In[2]:
+
+
+# df.columns = df.columns.str.strip()
+df.columns = df.columns.str.replace(' ','_')
+
+
+# In[4]:
+
+
+df = df.rename(columns=lambda x: x.replace('#', 'number'))
+
+
+# In[5]:
+
+
+df
+
+
+# In[13]:
+
+
+df = df.replace(['*****', np.NaN], 'null')
+
+
+# In[14]:
+
+
+df
+
+
+# In[21]:
+
+
+output = df.to_json(path_or_buf='master_major_page_data.json', orient='records' , lines=True)
+
