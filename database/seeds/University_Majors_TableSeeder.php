e9f9268646adb581dfcb6531ea96c4a0a7507def
ChanM0 <mikec201722@gmail.com>
2018-08-22 12:31:36 -0700

    Edit University majors table seeder

--- database/seeds/University_Majors_TableSeeder.php
+++ database/seeds/University_Majors_TableSeeder.php
@@ -12,14 +12,19 @@ class University_Majors_TableSeeder extends Seeder
      */
     public function run()
     {
-        $json = File::get('database/data/northridge_hegis_codes.json');
-        $data = json_decode($json);
-        $northridgeUniversityCode = 70;
-        foreach($data as $row){
-            $universityMajor = new UniversityMajor();
-            $universityMajor->hegis_code = $row->hegis_code;
-            $universityMajor->university_id = $northridgeUniversityCode;
-            $universityMajor->save();
+        $jsonCollection = array();
+        $json = File::get("database/data/northridge_university_majors_id.json");
+        array_push($jsonCollection,$json);
+
+        foreach ($jsonCollection as $json ){
+            $data = json_decode($json);
+            $index = 1;
+            foreach($data as $row){
+                $universityMajor = new UniversityMajor();
+                $universityMajor->hegis_code = $row->hegis_code;
+                $universityMajor->university_id = $northridgeUniversityCode;
+                $universityMajor->save();
+            }
         }
     }
 }
