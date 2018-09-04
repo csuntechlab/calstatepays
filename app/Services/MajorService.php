8a3fd3ed1fe156b85484a75550946efca542c617
ChanM0 <mikec201722@gmail.com>
2018-09-04 11:45:11 -0700

    Broke Code.

--- app/Services/MajorService.php
+++ app/Services/MajorService.php
@@ -55,11 +55,16 @@ class MajorService implements MajorContract
         return $hegis_code;
     }
 
-    public function getUniversityMajorId($hegisCode, $universityId)
+    public function getUniversityMajorId($hegisCode, $universityId, $major)
     {
+        var_dump( $hegisCode );
+        var_dump( $universityId );
+        var_dump( $major );
         $universityMajorId = UniversityMajor::where('hegis_code', $hegisCode)
-                                                ->where('university_id', $universityId)
-                                                ->first(['id']);
+                                                ->where('university_id', $universityId)->get();
+                                                // ->where('university_major',$major)
+                                                // ->first(['id']);
+        var_dump($universityMajorId);                                               
         return $universityMajorId->id;
     }
 
--- database/seeds/University_Majors_TableSeeder.php
+++ database/seeds/University_Majors_TableSeeder.php
@@ -12,39 +12,22 @@ class University_Majors_TableSeeder extends Seeder
      */
     public function run()
     {
-        $jsonCollection = array();
-        $json = File::get("database/data/northridge_university_majors_id.json");
-        array_push($jsonCollection,$json);
-
-        $json = File::get("database/data/channel_islands_university_majors_id.json");
-        array_push($jsonCollection,$json);
-
-        $json = File::get("database/data/dominguez_hills_university_majors_id.json");
-        array_push($jsonCollection,$json);
-
-        $json = File::get("database/data/fullerton_university_majors_id.json");
-        array_push($jsonCollection,$json);
-        
-        $json = File::get("database/data/los_angeles_university_majors_id.json");
-        array_push($jsonCollection,$json);
-        
-        $json = File::get("database/data/pomona_university_majors_id.json");
-        array_push($jsonCollection,$json);
-
+        $json = File::get("database/data/master_university_table.json");
+        $data = json_decode($json);
+        // var_dump($json);
+                
         // $json = File::get("database/data/long_beach_university_majors_id.json");
         // array_push($jsonCollection,$json);
 
-        foreach ($jsonCollection as $json ){
-            $data = json_decode($json);
-            $index = 1;
-            // var_dump($data);
-            foreach($data as $row){
-                $universityMajor = new UniversityMajor();
-                $universityMajor->hegis_code = $row->hegis_codes;
-                $universityMajor->university_id = $row->university_id;
-                $universityMajor->id = $row->id;
-                $universityMajor->save();
-            }
+        foreach($data as $row) {
+
+            $universityMajor = new UniversityMajor();
+            $universityMajor->hegis_code = $row->hegis_codes;
+            $universityMajor->university_id = $row->university_id;
+            $universityMajor->university_major = $row->major;
+            $universityMajor->id = $row->id;
+            $universityMajor->save();
         }
+        
     }
 }
