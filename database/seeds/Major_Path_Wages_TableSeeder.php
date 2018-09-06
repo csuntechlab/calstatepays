99b5c02e6ba5c577fa20a69afb308bcb085e6c5e
carlosbenavides123 <carlos7legend@gmail.com>
2018-09-06 09:41:17 -0700

    answer to major path id problem

--- database/seeds/Major_Path_Wages_TableSeeder.php
+++ database/seeds/Major_Path_Wages_TableSeeder.php
@@ -35,13 +35,28 @@ class Major_Path_Wages_TableSeeder extends Seeder
         // $json = File::get("database/data/long_beach_majors_path_wages.json");
         // array_push($jsonCollection,$json);
 
+        /**
+         * Try to figure out a way in python such that we can make a master major path wage table?
+         *  The issue was that when we query
+         *  http://127.0.0.1:8000/api/major/5131/10
+         *  the way our code works, it wants to find the register/address of 29011
+         *  and our major_path_id weren't adding up to a summation
+         *  for example pomona only goes from major_path_id 1 to 9k (in the json)
+         *  it would be impossible to even hit that
+         *  so the solution is to make the json auto increment for major_path_id
+         *  We can probably do this in python no problem
+         */
+
+        $i = 1;
+
         foreach ($jsonCollection as $json ){
             $data = json_decode($json);
 
             foreach($data as $row){
+                $i++;
                 $majorPathWage = new MajorPathWage();
 
-                $majorPathWage->major_path_id = $row->major_path_id;
+                $majorPathWage->major_path_id = $i;
                 $majorPathWage->_25th = $row->_25th_percentile_earnings;
                 $majorPathWage->_50th = $row->_50th_percentile_earnings;
                 $majorPathWage->_75th = $row->_75th_percentile_earnings;
