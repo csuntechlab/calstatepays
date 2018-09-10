285396d47ef763d43ef629f1398c75fb1e10dbb0
carlosbenavides123 <carlos7legend@gmail.com>
2018-09-10 15:54:51 -0700

    scared to remove lol

--- tests/Services/MajorServiceTest.php
+++ tests/Services/MajorServiceTest.php
@@ -91,9 +91,10 @@ class MajorServiceTest extends TestCase
          $this->arrayHasKey("roi", $response);
      }
 
-     public function test_getAllUniversities_returns_7_univerities()
-     {
-         $response = $this->majorService->getAllUniversities();
-         $this->assertEquals(University::count(), count($response));
-     }
+//      this method no longer exists
+//     public function test_getAllUniversities_returns_7_univerities()
+//     {
+//         $response = $this->majorService->getAllUniversities();
+//         $this->assertEquals(University::count(), count($response));
+//     }
 }
