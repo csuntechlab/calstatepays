70f3053c503ba3ee21f3502b04c849db5dead472
ChanM0 <mikec201722@gmail.com>
2018-10-02 12:53:46 -0700

    Fix industry test Industry test now takes in account for Helper Contract

--- app/Services/IndustryService.php
+++ app/Services/IndustryService.php
@@ -44,12 +44,67 @@ class IndustryService implements IndustryContract
                     ->where('university_id', $university_id)
                     ->firstOrFail();
 
-        $industry_populations= $this->helperRetriever->sortIndustryPopulation($university_major);
+        $industry_populations= $this->sortIndustryPopulation($university_major);
         
-        $population_total = $this->helperRetriever->getIndustryPopulationTotals($industry_populations);
+        $population_total = $this->getIndustryPopulationTotals($industry_populations);
         
-        $industry_populations = $this->helperRetriever->calculatePopulationPercentagesAndReturnImages($industry_populations, $population_total);
+        $industry_populations = $this->calculatePopulationPercentagesAndReturnImages($industry_populations, $population_total);
 
         return $industry_populations;
+    }
+
+    private function sortIndustryPopulation($university_major)
+    {
+        $industry_populations = $university_major->industryPathTypes->sortByDesc('population.population_found')->values();
+        return $industry_populations;
+    } 
+
+    private function getIndustryPopulationTotals($industry_populations) {
+        $total = 0;
+        foreach($industry_populations as $pop) {
+            if($pop->population->population_found != null){
+                $total += $pop->population->population_found;
+            }
+        }
+        return $total;
+    }
+
+    private function calculatePopulationPercentagesAndReturnImages($industry_populations, $population_total) {
+        $final =  $industry_populations = $industry_populations
+            ->map(function ($industry,$index = 0) use($population_total){
+                $index++;
+                $percentage = $this->populationHandler($industry,$population_total);
+                return [
+                    'title' => $industry->naicsTitle->naics_title,
+                    'percentage' => $percentage,
+                    'rank' => $index,
+                    'image' => asset($industry->naicsTitle->image),
+                    'industryWage' => $industry->industryWage->avg_annual_wage_5
+                ];
+            });
+        return $final;
+    }
+
+    private function populationHandler($industry, $population_total){
+        if( ($industry->population->population_found!=null) && ($population_total != null) ){
+            $percentage = round( ($industry->population->population_found/$population_total)*100, 0, PHP_ROUND_HALF_DOWN);
+            return $percentage;
+        }
+        return null;
+    }
+
+    private function calculatePopulationPercentages($industry_populations, $population_total) {
+        $final =  $industry_populations = $industry_populations
+            ->map(function ($industry,$index = 0) use($population_total){
+                $index++;
+                $percentage = $this->populationHandler($industry,$population_total);
+                return [
+                    'title' => $industry->naicsTitle->naics_title,
+                    'percentage' => $percentage,
+                    'rank' => $index,
+                    'industryWage' => $industry->industryWage->avg_annual_wage_5
+                ];
+            });
+        return $final;
     }
 }
--- tests/Services/IndustryServiceTest.php
+++ tests/Services/IndustryServiceTest.php
@@ -3,25 +3,30 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\TestCase;
 use App\Services\IndustryService;
+use App\Contracts\HelperContract;
+use Mockery;
+use Tests\TestCase;
+
 
 class IndustryServiceTest extends TestCase
 {
     use DatabaseMigrations;
     protected $industryService;
+    protected $helperRetriever;
 
     public function setUp()
     {
         parent::setUp();
-        $this->industryService= new IndustryService();
+        $this->helperRetriever = Mockery::spy(HelperContract::class);
         $this->seed('Naics_Titles_TableSeeder');
     }
 
     public function test_getAllIndustryNaicsTitles_returns_all_rows()
     {
         // route is api/industry/naics-titles
-        $response = $this->industryService->getAllIndustryNaicsTitles();
+        $industryService = new IndustryService($this->helperRetriever);
+        $response = $industryService->getAllIndustryNaicsTitles();
         $this->assertArrayHasKey("naics_code", $response[0]);    
         $this->assertArrayHasKey("title", $response[0]);        
         $this->assertArrayHasKey('image', $response[0]);
@@ -31,12 +36,30 @@ class IndustryServiceTest extends TestCase
      {
          // route is api/industry/{hegis_code}/{university_id}
          // i.e. api/industry/5021/70
-         $this->seed('University_Majors_TableSeeder');
-         $this->seed('Master_Industry_Path_Types_Table_Seeder');
-         $this->seed('Master_Industry_Wages_Table_Seeder');
-         $this->seed('Population_Table_Seeder');
-         $this->seed('Universities_TableSeeder');
-         $response = $this->industryService->getIndustryPopulationByRank(22111, 70);
+
+        $this->seed('University_Majors_TableSeeder');
+        $this->seed('Master_Industry_Path_Types_Table_Seeder');
+        $this->seed('Master_Industry_Wages_Table_Seeder');
+        $this->seed('Population_Table_Seeder');
+        $this->seed('Universities_TableSeeder');
+
+        $opt_in_data = [
+            "id" => "70",
+            "university_name" => "Northridge",
+            "opt_in" => 1
+         ];
+
+         $university_id = 70;
+
+         $this->helperRetriever
+            ->shouldReceive('checkOptIn')
+            ->once()
+            ->with($university_id)
+            ->andReturn($opt_in_data);
+
+        $industryService = new IndustryService($this->helperRetriever);
+
+         $response = $industryService->getIndustryPopulationByRank(22111, 70);
          $this->assertArrayHasKey("title", $response[0]);
          $this->assertArrayHasKey("percentage", $response[0]);
          $this->assertArrayHasKey('rank', $response[0]);
