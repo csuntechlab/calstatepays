9b417e7aec8981ee9ce873a8d0ca826a2a5c21c7
ChanM0 <mikec201722@gmail.com>
2018-10-02 10:46:11 -0700

    Made functions more modular All the helper functions are moved to the HelperService

--- app/Services/HelperService.php
+++ app/Services/HelperService.php
@@ -14,15 +14,13 @@ class HelperService
         University::where('id',$university_id)->where('opt_in',1)->firstOrFail();
     }
 
-    public function handlePopulationCalculation($university_major)
+    public function sortIndustryPopulation($university_major)
     {
         $industry_populations = $university_major->industryPathTypes->sortByDesc('population.population_found')->values();
-        $population_total = $this->getIndustryPopulationTotals($industry_populations);
-        $industry_populations = $this->calculatePopulationPercentages($industry_populations, $population_total);
         return $industry_populations;
-    }
+    } 
 
-    private function getIndustryPopulationTotals($industry_populations) {
+    public function getIndustryPopulationTotals($industry_populations) {
         $total = 0;
         foreach($industry_populations as $pop) {
             if($pop->population->population_found != null){
@@ -32,7 +30,7 @@ class HelperService
         return $total;
     }
 
-    private function calculatePopulationPercentages($industry_populations, $population_total) {
+    public function calculatePopulationPercentagesAndReturnImages($industry_populations, $population_total) {
         $final =  $industry_populations = $industry_populations
             ->map(function ($industry,$index = 0) use($population_total){
                 $index++;
@@ -48,11 +46,26 @@ class HelperService
         return $final;
     }
 
-    private function populationHandler($industry, $population_total){
+    public function populationHandler($industry, $population_total){
         if( ($industry->population->population_found!=null) && ($population_total != null) ){
             $percentage = round( ($industry->population->population_found/$population_total)*100, 0, PHP_ROUND_HALF_DOWN);
             return $percentage;
         }
         return null;
+    }
+
+    public function calculatePopulationPercentages($industry_populations, $population_total) {
+        $final =  $industry_populations = $industry_populations
+            ->map(function ($industry,$index = 0) use($population_total){
+                $index++;
+                $percentage = $this->populationHandler($industry,$population_total);
+                return [
+                    'title' => $industry->naicsTitle->naics_title,
+                    'percentage' => $percentage,
+                    'rank' => $index,
+                    'industryWage' => $industry->industryWage->avg_annual_wage_5
+                ];
+            });
+        return $final;
     }
 }
--- app/Services/IndustryService.php
+++ app/Services/IndustryService.php
@@ -28,27 +28,13 @@ class IndustryService implements IndustryContract
         return $allNaicsTitles;
     }
 
-    public function getIndustryPopulationAndWage($hegis_code, $university_id)
-    {
-        $this->helper->checkOptIn($university_id);
-
-        $university_major = UniversityMajor::with(['industryPathTypes',
-                    'industryPathTypes.population', 'industryPathTypes.naicsTitle','industryPathTypes.industryWage'])
-                    ->where('hegis_code', $hegis_code)
-                    ->where('university_id', $university_id)
-                    ->firstOrFail();
-
-        return $this->helper->handlePopulationCalculation($university_major);
-
-    }
-
     public function getIndustryPopulationByRank($hegis_code, $university_id)
     {
         /**
          *  Would here be the best choice to check if CSU Opts In or Not?
          */
         $this->helper->checkOptIn($university_id);
-
+        
         $university_major = UniversityMajor::with(['industryPathTypes' => function ($query) {
                 $query->where('entry_status', 'FTF + FTT');
                 $query->where('student_path', 1);
@@ -56,7 +42,14 @@ class IndustryService implements IndustryContract
                     ->where('hegis_code', $hegis_code)
                     ->where('university_id', $university_id)
                     ->firstOrFail();
+        // dd($university_major);
 
-        return $this->helper->handlePopulationCalculation($university_major);
+        $industry_populations= $this->helper->sortIndustryPopulation($university_major);
+        
+        $population_total = $this->helper->getIndustryPopulationTotals($industry_populations);
+        
+        $industry_populations = $this->helper->calculatePopulationPercentagesAndReturnImages($industry_populations, $population_total);
+
+        return $industry_populations;
     }
 }
