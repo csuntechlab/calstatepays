975d04f974821631eee373b2fe57340c743a57bf
ChanM0 <mikec201722@gmail.com>
2018-10-18 09:08:13 -0700

    Able to print out csv's for power users

--- python_parser/python_parser_work_in_progress/csuMetro_Parsing/dataframeToCSV.py
+++ python_parser/python_parser_work_in_progress/csuMetro_Parsing/dataframeToCSV.py
@@ -7,6 +7,7 @@ class DfToCSV:
   
   def printCSV(self):
     aggregate = self.df.loc[self.df['campus'].isin([0])]
+    aggregate['campus'] = '10 + 40 + 45 + 50 + 55 + 70 + 73'
     aggregate.to_csv('./newCSV/aggregate'+self.name+'.csv', sep='\t', encoding='utf-8')
 
     northridge = self.df.loc[self.df['campus'].isin([70])]
