b54eff29e7848e950f97c8affd8a07204b7cebe3
chanM0 <mikec201722@gmail.com>
2018-10-26 10:26:06 -0700

    Found Error

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(Hegis_Codes_TableSeeder::class);
-        $this->call(University_Majors_TableSeeder::class);
+        // $this->call(University_Majors_TableSeeder::class);
         $this->call(Naics_Titles_TableSeeder::class);
         $this->call(Student_Paths_TableSeeder::class);
         $this->call(Field_Of_Studies_TableSeeder::class);
