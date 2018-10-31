d0f256df5449f530db6ec23da1ae05b1e7df6e96
carlosbenavides123 <carlos7legend@gmail.com>
2018-10-31 10:52:43 -0700

    need to merge dev

--- tests/Unit/IndustryControllerTest.php
+++ tests/Unit/IndustryControllerTest.php
@@ -233,6 +233,7 @@ class IndustryControllerTest extends TestCase
       }
 
       public function test_IndustryTestController(){
+
           $titles = $this->json('GET','api/industry/naics-titles');
           $titles = $titles->getOriginalContent();
           $this->assertArrayHasKey('naics_code', $titles[0]);
@@ -267,6 +268,32 @@ class IndustryControllerTest extends TestCase
             $this->assertEquals($data[$iterate]['title'], $success['title']);
             $this->assertEquals($data[$iterate]['industryWage'], $success['industryWage']);
           }
+
+          /** make a api industry api call with other api calls  */
+
+          $university_call = $this->json("GET","api/university");
+          $university_call = $university_call->getOriginalContent();
+
+          /** test with northridge */
+          $northridge = $university_call[5];
+          $northridge_short_name = $northridge['short_name'];
+
+          /** get hegis codes */
+          $northridge_hegis = $this->json("GET","api/major/hegis-codes/university/".$northridge_short_name);
+          $northridge_hegis = $northridge_hegis->getOriginalContent();
+
+          $northridge_accounting_hegis = $northridge_hegis[0]['hegis_code'];
+
+          /** call industry wages */
+          $northridge_accounting_wages = $this->json("GET","api/industry/".$northridge_accounting_hegis."/".$northridge_short_name."/1");
+          $northridge_accounting_wages = $northridge_accounting_wages->getOriginalContent();
+          $northridge_accounting_wages = json_encode($northridge_accounting_wages);
+
+
+          $northridge_accounting_wages_live_data = json_encode([[  "title"=> "Professional, Scientific, & Technical Skills",  "percentage"=> 40,  "rank"=> 1,  "industryWage"=> null],[  "title"=> "Finance & Insurance",  "percentage"=> 11,  "rank"=> 2,  "industryWage"=> null],[  "title"=> "Information",  "percentage"=> 6,  "rank"=> 3,  "industryWage"=> null],[  "title"=> "Admin & Support & Waste Mgmt & Remediation",  "percentage"=> 6,  "rank"=> 4,  "industryWage"=> null],[  "title"=> "Wages - No NAICS Code",  "percentage"=> 5,  "rank"=> 5,  "industryWage"=> 48541],[  "title"=> "Government",  "percentage"=> 5,  "rank"=> 6,  "industryWage"=> null],[  "title"=> "Manufacturing",  "percentage"=> 4,  "rank"=> 7,  "industryWage"=> null],[  "title"=> "Real Estate & Rental & Leasing",  "percentage"=> 4,  "rank"=> 8,  "industryWage"=> null],[  "title"=> "Health Care & Social Assistance",  "percentage"=> 3,  "rank"=> 9,  "industryWage"=> null],[  "title"=> "Retail Trade",  "percentage"=> 3,  "rank"=> 10,  "industryWage"=> 26395],[  "title"=> "Wholesale Trade",  "percentage"=> 3,  "rank"=> 11,  "industryWage"=> 63012],[  "title"=> "Educational Services",  "percentage"=> 2,  "rank"=> 12,  "industryWage"=> 12295],[  "title"=> "Other Services",  "percentage"=> 1,  "rank"=> 13,  "industryWage"=> null],[  "title"=> "Construction",  "percentage"=> 1,  "rank"=> 14,  "industryWage"=> null],[  "title"=> "Accommodation & Food Services",  "percentage"=> 1,  "rank"=> 15,  "industryWage"=> null],[  "title"=> "Transportation & Warehousing",  "percentage"=> 1,  "rank"=> 16,  "industryWage"=> 44667],[  "title"=> "Arts, Entertainment, & Recreation",  "percentage"=> 1,  "rank"=> 17,  "industryWage"=> 49380],[  "title"=> "Management of Companies & Enterprises",  "percentage"=> 1,  "rank"=> 18,  "industryWage"=> 79253],[  "title"=> "Utilities",  "percentage"=> 0,  "rank"=> 19,  "industryWage"=> null],[  "title"=> "Agriculture, Forestry, Fishing, & Hunting",  "percentage"=> 0,  "rank"=> 20,  "industryWage"=> 44893],[  "title"=> "No Wages",  "percentage"=> 0,  "rank"=> 21,  "industryWage"=> null],[  "title"=> "Mining",  "percentage"=> null,  "rank"=> 22,  "industryWage"=> null],[  "title"=> "Non-Classified",  "percentage"=> null,  "rank"=> 23,  "industryWage"=> null],[  "title"=> "Professional, Scientific, & Technical Skills",  "percentage"=> 40,  "rank"=> 1,  "industryWage"=> null],[  "title"=> "Finance & Insurance",  "percentage"=> 11,  "rank"=> 2,  "industryWage"=> null],[  "title"=> "Information",  "percentage"=> 6,  "rank"=> 3,  "industryWage"=> null],[  "title"=> "Admin & Support & Waste Mgmt & Remediation",  "percentage"=> 6,  "rank"=> 4,  "industryWage"=> null],[  "title"=> "Wages - No NAICS Code",  "percentage"=> 5,  "rank"=> 5,  "industryWage"=> 48541],[  "title"=> "Government",  "percentage"=> 5,  "rank"=> 6,  "industryWage"=> null],[  "title"=> "Manufacturing",  "percentage"=> 4,  "rank"=> 7,  "industryWage"=> null],[  "title"=> "Real Estate & Rental & Leasing",  "percentage"=> 4,  "rank"=> 8,  "industryWage"=> null],[  "title"=> "Health Care & Social Assistance",  "percentage"=> 3,  "rank"=> 9,  "industryWage"=> null],[  "title"=> "Retail Trade",  "percentage"=> 3,  "rank"=> 10,  "industryWage"=> 26395],[  "title"=> "Wholesale Trade",  "percentage"=> 3,  "rank"=> 11,  "industryWage"=> 63012],[  "title"=> "Educational Services",  "percentage"=> 2,  "rank"=> 12,  "industryWage"=> 12295],[  "title"=> "Other Services",  "percentage"=> 1,  "rank"=> 13,  "industryWage"=> null],[  "title"=> "Construction",  "percentage"=> 1,  "rank"=> 14,  "industryWage"=> null],[  "title"=> "Accommodation & Food Services",  "percentage"=> 1,  "rank"=> 15,  "industryWage"=> null],[  "title"=> "Transportation & Warehousing",  "percentage"=> 1,  "rank"=> 16,  "industryWage"=> 44667],[  "title"=> "Arts, Entertainment, & Recreation",  "percentage"=> 1,  "rank"=> 17,  "industryWage"=> 49380],[  "title"=> "Management of Companies & Enterprises",  "percentage"=> 1,  "rank"=> 18,  "industryWage"=> 79253],[  "title"=> "Utilities",  "percentage"=> 0,  "rank"=> 19,  "industryWage"=> null],[  "title"=> "Agriculture, Forestry, Fishing, & Hunting",  "percentage"=> 0,  "rank"=> 20,  "industryWage"=> 44893],[  "title"=> "No Wages",  "percentage"=> 0,  "rank"=> 21,  "industryWage"=> null],[  "title"=> "Mining",  "percentage"=> null,  "rank"=> 22,  "industryWage"=> null],[  "title"=> "Non-Classified",  "percentage"=> null,  "rank"=> 23,  "industryWage"=> null]]);
+
+          $this->assertEquals($northridge_accounting_wages,$northridge_accounting_wages_live_data);
+        //   dd($northridge_accounting_hegis);
 
 
         //   $major = $success[0]['']
