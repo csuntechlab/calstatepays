7569606d5585ca3be4ffbf9f2d8ffd488820b7ed
chanm0 <mikec201722@gmail.com>
2018-11-06 10:17:59 -0800

    found errors

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,32 +11,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(Hegis_Codes_TableSeeder::class);
-        $this->call(Naics_Titles_TableSeeder::class);
-        $this->call(Student_Paths_TableSeeder::class);
-        $this->call(Field_Of_Studies_TableSeeder::class);
-        $this->call(Hegis_Categories_TableSeeder::class);
-        $this->call(Universities_TableSeeder::class);
+        // $this->call(Hegis_Codes_TableSeeder::class);
+        // $this->call(Naics_Titles_TableSeeder::class);
+        // $this->call(Student_Paths_TableSeeder::class);
+        // $this->call(Field_Of_Studies_TableSeeder::class);
+        // $this->call(Hegis_Categories_TableSeeder::class);
+        // $this->call(Universities_TableSeeder::class);
 
-        $this->call(Aggregate_Industry_Path_Types_TableSeeder::class);
-        $this->call(Aggregate_Industry_Path_Wages_TableSeeder::class);
-        $this->call(Aggregate_Industry_Population_TableSeeder::class);
-        $this->call(Aggregate_Major_Path_TableSeeder::class);
-        $this->call(Aggregate_Major_Path_Wages_TableSeeder::class);
-        $this->call(Aggregate_University_Majors_TableSeeder::class);
+        // $this->call(Aggregate_Industry_Path_Types_TableSeeder::class);
+        // $this->call(Aggregate_Industry_Path_Wages_TableSeeder::class);
+        // $this->call(Aggregate_Industry_Population_TableSeeder::class);
+        // $this->call(Aggregate_Major_Path_TableSeeder::class);
+        // $this->call(Aggregate_Major_Path_Wages_TableSeeder::class);
+        // $this->call(Aggregate_University_Majors_TableSeeder::class);
 
-        $this->call(Northridge_Industry_Path_Types_TableSeeder::class);
-        $this->call(Northridge_Industry_Path_Wages_TableSeeder::class);
-        $this->call(Northridge_Industry_Population_TableSeeder::class);
-        $this->call(Northridge_Major_Path_TableSeeder::class);
-        $this->call(Northridge_Major_Path_Wages_TableSeeder::class);
-        $this->call(Northridge_University_Majors_TableSeeder::class);
+        // $this->call(Northridge_Industry_Path_Types_TableSeeder::class);
+        // $this->call(Northridge_Industry_Path_Wages_TableSeeder::class);
+        // $this->call(Northridge_Industry_Population_TableSeeder::class);
+        // $this->call(Northridge_Major_Path_TableSeeder::class);
+        // $this->call(Northridge_Major_Path_Wages_TableSeeder::class);
+        // $this->call(Northridge_University_Majors_TableSeeder::class);
 
         // $this->call(Master_FRE_Page_Data_TableSeeder::class);
 
-        // $this->call(Industry_Different_Hegis_Same_Majors_Table_Seeder::class);
-        // $this->call(Industry_Same_Hegis_Different_Majors_Table_Seeder::class);
-        // $this->call(Majors_Same_Hegis_Different_Major_Table_Seeder::class);
-        // $this->call(Majors_Same_Hegis_Different_Major_Table_Seeder::class);
+        $this->call(Industry_Different_Hegis_Same_Majors_Table_Seeder::class);
+        $this->call(Industry_Same_Hegis_Different_Majors_Table_Seeder::class);
+        $this->call(Majors_Same_Hegis_Different_Major_Table_Seeder::class);
+        $this->call(Majors_Different_Hegis_Same_Major_Table_Seeder::class);
     }
 }
