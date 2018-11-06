e281ea528c4a4ab4490f7ca05cd8cf51e97004b5
chanm0 <mikec201722@gmail.com>
2018-11-06 11:14:28 -0800

    Able to seed new data

--- python_parser/python_parser_work_in_progress/csuMetro_Parsing/UniversityMajorsTable.py
+++ python_parser/python_parser_work_in_progress/csuMetro_Parsing/UniversityMajorsTable.py
@@ -50,10 +50,12 @@ class UniversitiesDataFrameErrorChecker():
     df.columns = df.columns.str.replace(' ','_')
     
     #TODO: ERROR CHECKING COMMENT THIS OUT 
-    # df = df.loc[df['student_path'].isin([1,2,4])]
+    df = df.loc[df['student_path'].isin([1,2,4])]
+    df = df.loc[df['entry_stat'].isin(['FTF + FTT'])]
+
+    #TODO: ERROR CHECKING COMMENT THIS Out for error checking 
     # df = df.loc[df['student_path'].isin([5,6])]
     # df = df.loc[df['student_path'].isin([1,2,3,4])]
-    df = df.loc[df['entry_stat'].isin(['FTF + FTT'])]
     return df
 
   def create_base_university_majors_table(self,df):
