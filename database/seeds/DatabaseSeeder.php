48344bb9d6781f97f217dec8e37e8385f43b85be
chanm0 <mikec201722@gmail.com>
2018-11-09 11:14:25 -0800

    Able to seed most of the data

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -33,12 +33,12 @@ class DatabaseSeeder extends Seeder
         $this->call(Northridge_Major_Path_Wages_TableSeeder::class);
         $this->call(Northridge_University_Majors_TableSeeder::class);
 
-        // $this->call(Channel_Islands_Industry_Path_Types_TableSeeder::class);
-        // $this->call(Channel_Islands_Industry_Path_Wages_TableSeeder::class);
-        // $this->call(Channel_Islands_Industry_Population_TableSeeder::class);
-        // $this->call(Channel_Islands_Major_Path_TableSeeder::class);
-        // $this->call(Channel_Islands_Major_Path_Wages_TableSeeder::class);
-        // $this->call(Channel_Islands_University_Majors_TableSeeder::class);
+        $this->call(Channel_Islands_Industry_Path_Types_TableSeeder::class);
+        $this->call(Channel_Islands_Industry_Path_Wages_TableSeeder::class);
+        $this->call(Channel_Islands_Industry_Population_TableSeeder::class);
+        $this->call(Channel_Islands_Major_Path_TableSeeder::class);
+        $this->call(Channel_Islands_Major_Path_Wages_TableSeeder::class);
+        $this->call(Channel_Islands_University_Majors_TableSeeder::class);
 
         // $this->call(Dominguez_Hills_Industry_Path_Types_TableSeeder::class);
         // $this->call(Dominguez_Hills_Industry_Path_Wages_TableSeeder::class);
@@ -47,33 +47,33 @@ class DatabaseSeeder extends Seeder
         // $this->call(Dominguez_Hills_Major_Path_Wages_TableSeeder::class);
         // $this->call(Dominguez_Hills_University_Majors_TableSeeder::class);
 
-        // $this->call(Fullerton_Industry_Path_Types_TableSeeder::class);
-        // $this->call(Fullerton_Industry_Path_Wages_TableSeeder::class);
-        // $this->call(Fullerton_Industry_Population_TableSeeder::class);
-        // $this->call(Fullerton_Major_Path_TableSeeder::class);
-        // $this->call(Fullerton_Major_Path_Wages_TableSeeder::class);
-        // $this->call(Fullerton_University_Majors_TableSeeder::class);
+        $this->call(Fullerton_Industry_Path_Types_TableSeeder::class);
+        $this->call(Fullerton_Industry_Path_Wages_TableSeeder::class);
+        $this->call(Fullerton_Industry_Population_TableSeeder::class);
+        $this->call(Fullerton_Major_Path_TableSeeder::class);
+        $this->call(Fullerton_Major_Path_Wages_TableSeeder::class);
+        $this->call(Fullerton_University_Majors_TableSeeder::class);
 
-        // $this->call(Long_Beach_Industry_Path_Types_TableSeeder::class);
-        // $this->call(Long_Beach_Industry_Path_Wages_TableSeeder::class);
-        // $this->call(Long_Beach_Industry_Population_TableSeeder::class);
-        // $this->call(Long_Beach_Major_Path_TableSeeder::class);
-        // $this->call(Long_Beach_Major_Path_Wages_TableSeeder::class);
-        // $this->call(Long_Beach_University_Majors_TableSeeder::class);
+        $this->call(Long_Beach_Industry_Path_Types_TableSeeder::class);
+        $this->call(Long_Beach_Industry_Path_Wages_TableSeeder::class);
+        $this->call(Long_Beach_Industry_Population_TableSeeder::class);
+        $this->call(Long_Beach_Major_Path_TableSeeder::class);
+        $this->call(Long_Beach_Major_Path_Wages_TableSeeder::class);
+        $this->call(Long_Beach_University_Majors_TableSeeder::class);
 
-        // $this->call(Los_Angeles_Industry_Path_Types_TableSeeder::class);
-        // $this->call(Los_Angeles_Industry_Path_Wages_TableSeeder::class);
-        // $this->call(Los_Angeles_Industry_Population_TableSeeder::class);
-        // $this->call(Los_Angeles_Major_Path_TableSeeder::class);
-        // $this->call(Los_Angeles_Major_Path_Wages_TableSeeder::class);
-        // $this->call(Los_Angeles_University_Majors_TableSeeder::class);
+        $this->call(Los_Angeles_Industry_Path_Types_TableSeeder::class);
+        $this->call(Los_Angeles_Industry_Path_Wages_TableSeeder::class);
+        $this->call(Los_Angeles_Industry_Population_TableSeeder::class);
+        $this->call(Los_Angeles_Major_Path_TableSeeder::class);
+        $this->call(Los_Angeles_Major_Path_Wages_TableSeeder::class);
+        $this->call(Los_Angeles_University_Majors_TableSeeder::class);
 
-        // $this->call(Pomona_Industry_Path_Types_TableSeeder::class);
-        // $this->call(Pomona_Industry_Path_Wages_TableSeeder::class);
-        // $this->call(Pomona_Industry_Population_TableSeeder::class);
-        // $this->call(Pomona_Major_Path_TableSeeder::class);
-        // $this->call(Pomona_Major_Path_Wages_TableSeeder::class);
-        // $this->call(Pomona_University_Majors_TableSeeder::class);
+        $this->call(Pomona_Industry_Path_Types_TableSeeder::class);
+        $this->call(Pomona_Industry_Path_Wages_TableSeeder::class);
+        $this->call(Pomona_Industry_Population_TableSeeder::class);
+        $this->call(Pomona_Major_Path_TableSeeder::class);
+        $this->call(Pomona_Major_Path_Wages_TableSeeder::class);
+        $this->call(Pomona_University_Majors_TableSeeder::class);
 
         // $this->call(Master_FRE_Page_Data_TableSeeder::class);
 
