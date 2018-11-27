0c2aab65b10e1213ee6705e87e002556cb8b4a2e
ekno27 <edgkano@gmail.com>
2018-11-27 11:38:12 -0800

    Fixed sub-nav issue

--- resources/src/js/components/global/sub-nav.vue
+++ resources/src/js/components/global/sub-nav.vue
@@ -1,5 +1,5 @@
 <template>
-    <nav class="sub-nav container-fluid">
+    <nav class="sub-nav container-fluid" v-if="(innerHeight) > (deviceHeight/2)">
         <div class="row">
             <router-link class="col-4 sub-nav__element" active-class="sub-nav__element--active" to="/data/majors" @click.native="setDataPage('majors')">
             <i class="mobile-icon fa fa-cog fa-fw fa-graduation-cap fa-2x" aria-hidden="true"></i>
@@ -19,11 +19,25 @@
 <script>
 import {mapActions} from 'vuex';
 export default{
+    data() {
+        return {
+            deviceHeight: 0,
+            innerHeight: 0
+        }
+    },
     created: function() {
         this.toggleShowNavOnLoad()
     },
     methods: {
         ...mapActions(['setDataPage']),
+        getDeviceHeight(){
+            console.log("getting device");
+            this.deviceHeight = window.innerHeight;
+        },
+        getWindowHeight(event) {
+            console.log(window.innerHeight)
+            this.innerHeight = window.innerHeight
+        },
         toggleShowNavOnLoad() {
             var URL = window.location.href;
             if (URL.includes("industries")) {
@@ -31,7 +45,18 @@ export default{
             } else if (URL.includes("pfre")) {
                 this.setDataPage("pfre");
             }
-        }
+        },
+        inputIsFocused() {}
+    },
+    mounted() {
+        this.getDeviceHeight();
+        this.$nextTick(function(){
+            window.addEventListener("resize", this.getWindowHeight);
+            this.getWindowHeight();
+        })
+    },
+    beforeDestroy() {
+        window.removeEventListener("resize", this.getWindowHeight);
     }
 }
 </script>
