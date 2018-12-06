3fe41878e7f993db9a3df1fff68410b61ca80487
Jaime Garcia Garcia <jaime.garciagarcia@metalab.csun.edu>
2018-12-06 12:37:18 -0800

    made the avaiable campuses components

---
+++ resources/src/js/components/research/active-csu-tile.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
+        <div class="CSUTile">
+            <div class="CSUTile__Image" v-bind:style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.65)), url(' + campus + ')' }"></div>
+            <h3 class="CSUTile__CSUName">{{ title }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+/*
+@click="displayModal = true; id=0;"
+*/
+    import {mapGetters,mapActions} from 'vuex';
+    export default {
+        props: ["campus","title"],
+        data(){
+            return {
+            }
+        }
+        
+    }
+</script>
--- resources/src/js/router/views/research/index.vue
+++ resources/src/js/router/views/research/index.vue
@@ -1,4 +1,4 @@
- <template>
+<template>
     <div>
         <power-banner/>
         <power-users-modal :showModal=displayModal :university=university  v-on:closeModal="closeModal($event)"></power-users-modal>
@@ -12,47 +12,41 @@
                     </div>
                 </div>
                 <div class="row justify-content-start justify-content-xl-center">
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
-                            <div id="allcampus" class="campus not-avail" @click="displayModal = true; id=0;">
-                                <h3>Aggregate Data Across the 7 CSUs</h3>
-                            </div>
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
-                                <div id="csun" class="campus" @click="displayModal = true ; id=70  ;">
-                                    <h3>California State University Northridge</h3>
-                                </div>
+                    <active-c-s-u-tile :campus="campus.AllCSU.img" :title="campus.AllCSU.title"/>
+                    <active-c-s-u-tile :campus="campus.CSUN.img" :title="campus.CSUN.title"/>
+                    <!-- <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
+                        <div class="CSUTile--Disable">
+                            <div id="csulb" >
+                                <p>Data Not Available At This Time</p>
+                                <h3 class="CSUTile__CSUName">California State University Long Beach</h3>
+                            </div> 
                         </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
-                        <div id="csulb" class="campus not-avail">
-                            <p>Data Not Available At This Time</p>
-                            <h3>California State University Long Beach</h3>
-                        </div> 
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
                         <div id="csula" class=" campus not-avail">
-                            <p>Data Not Available At This Time</p>
-                            <h3>California State University Los Angeles</h3>
-                        </div>  
-                    </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
+                        </div>
+                        <p>Data Not Available At This Time</p>
+                        <h3>California State University Los Angeles</h3>  
+                    </div> -->
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
                         <div id="csuf" class=" campus not-avail">
                             <p>Data Not Available At This Time</p>
                             <h3>California State University Fullerton</h3>
                         </div> 
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
                         <div id="csudh" class=" campus not-avail">
                             <p>Data Not Available At This Time</p>
                             <h3>California State University Dominguez Hills</h3>
                         </div>  
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
                         <div id="csuci" class=" campus not-avail">
                             <p>Data Not Available At This Time</p>
                             <h3>California State University Channel Island</h3>
                         </div>
                     </div>
-                    <div class="col-12 col-sm-12 col-md-6 col-lg-4 col-xl-3 py-3">
+                    <div class="col-12 col-sm-6 col-md-6 col-lg-4 col-xl-3 py-3">
                         <div id="csup" class=" campus not-avail">
                             <p>Data Not Available At This Time</p>
                             <h3>California State University Pomona</h3>
@@ -66,6 +60,7 @@
 <script>
 import powerBanner from '../../../components/research/power-banner'
 import powerUsersModal from '../../../components/research/power-users-modal'
+import ActiveCSUTile from '../../../components/research/active-csu-tile'
 import {mapGetters} from 'vuex';
 export default {
     data(){
@@ -74,22 +69,34 @@ export default {
             // universityName:'',
             universityLink:'',
             selectedUniversity:'',
-            id:0
+            id:0,
+            campus:{
+                    AllCSU: {
+                        img: window.baseUrl  + '/img/csucampuses/allcsu.png',
+                        title: 'Aggregate Data Across the 7 CSUs'
+                    },
+                    CSUN: {
+                        img: window.baseUrl + '/img/csucampuses/northridge.png',
+                        title: 'California State University Northridge'
+                    } 
+            }
         }
-    },methods:{
+    },
+    methods:{
         closeModal(){
             this.displayModal = false;
         }
-    },computed:{
+    },
+    computed:{
         ...mapGetters(['universityById']),
         university(){
             return this.universityById(this.id)
         }
     },
-    
-  components: {
-      powerBanner,
-      powerUsersModal
-  }
+    components: {
+        powerBanner,
+        powerUsersModal,
+        ActiveCSUTile
+    }
 };
 </script>
