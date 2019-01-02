8dc3c3ace783141e4d9870e04fc9b339bd097f04
carlosbenavides123 <carlos7legend@gmail.com>
2019-01-02 11:47:00 -0800

    fix api-state names

--- app/Services/UniversityService.php
+++ app/Services/UniversityService.php
@@ -11,7 +11,18 @@ class UniversityService implements UniversityContract
 {
     public function getAllUniversities()
     {
-        $data = University::all()->toArray();
+        $data = University::all();
+
+        $data = $data->map(function ($item) {
+            return [
+                'id' => $item['id'],
+                'name' => $item['university_name'],
+                'short_name' => $item['short_name'],
+                'acronym' => $item['acronym'],
+                'opt_in' => $item['opt_in']
+            ];
+        });
+
         return $data;
     }
 }
