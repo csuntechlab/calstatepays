1ddc08a8320767378643a6b2c2928c3cc74b5596
chanm0 <mikec201722@gmail.com>
2019-01-25 12:53:33 -0800

    Able to add all values

--- database/seeds/PowerUsers/Power_User_Data_Los_Angeles_TableSeeder.php
+++ database/seeds/PowerUsers/Power_User_Data_Los_Angeles_TableSeeder.php
@@ -17,6 +17,7 @@ class Power_User_Data_Los_Angeles_TableSeeder extends Seeder
             DB::table('power_users_data')->insert([
                 'university_id' => $row->university_id,
                 'path_id' => $row->path_id,
+                'iframe_server' => $row->iframe_server,
                 'iframe_string' => $row->iframe_string,
                 'opt_in' => $row->opt_in
             ]);
