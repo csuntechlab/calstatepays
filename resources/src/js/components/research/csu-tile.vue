3eeb7e9ace4e3ef2110de787d7fb86c21e720827
luisjg <luis.guzman@metalab.csun.edu>
2019-03-14 10:05:42 -0700

    Fix the poweruser cards.

--- resources/src/js/components/research/csu-tile.vue
+++ resources/src/js/components/research/csu-tile.vue
@@ -1,10 +1,10 @@
 <template>
     <div class="col-12 col-md-6 col-lg-4 col-xl-3 py-3">
             <div class="CSUTile">
-                <div v-if="optIn === '1'" class="CSUTile__Image CSUTile__Image--Active"  v-bind:style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url(' + campusImg + ')' }"></div>
+                <div v-if="optIn === 1" class="CSUTile__Image CSUTile__Image--Active"  v-bind:style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.8)), url(' + campusImg + ')' }"></div>
                 <div v-else class="CSUTile__Image CSUTile__Image--Opt"  v-bind:style="{ backgroundImage: 'linear-gradient(rgba(0, 0, 0, 0.8), rgba(0, 0, 0, 0.8)), url(' + campusImg + ')' }"></div>
-                <h3 :class="[{ 'CSUTile__CSUName CSUTile__CSUName--Opt': optIn === '0' }, 'CSUTile__CSUName']">{{ title }}</h3>
-                <div v-if="optIn === '0'">
+                <h3 :class="[{ 'CSUTile__CSUName CSUTile__CSUName--Opt': optIn === 0 }, 'CSUTile__CSUName']">{{ title }}</h3>
+                <div v-if="optIn === 0">
                     <p class="CSUTile__DisableMessage">Data Not Available At This Time</p>
                 </div>
         </div>
