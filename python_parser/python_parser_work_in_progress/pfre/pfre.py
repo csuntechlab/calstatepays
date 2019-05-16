b4905647aff0d16be9aa9d04704ae5a58042b79d
chanm0 <mikec201722@gmail.com>
2019-05-16 10:45:25 -0700

    able to create a json file

--- python_parser/python_parser_work_in_progress/pfre/pfre.py
+++ python_parser/python_parser_work_in_progress/pfre/pfre.py
@@ -2,6 +2,8 @@ import os
 from os import listdir
 from os.path import isfile, join
 import pandas as pd
+import json
+import simplejson
 
 def main():
 	path = os.getcwd()
@@ -17,9 +19,15 @@ def main():
 	y = pd.read_csv(csvFiles[1])
 	y = y[['guid', 'entry_status', 'major', 'in_school_earning', 'fin_aid_0', 'fin_aid_3000', 'fin_aid_10000']]
 	res = x.append(y)
+	filePath = '../../../database/data/pfre_data.json'
+	
 	print(res)
 	res.to_csv("Pfre.csv", sep='\t', encoding='utf-8', index=False)
-
+	res = res.to_dict(orient='record')
+	with open (filePath, 'w' ) as fp:
+		fp.write(simplejson.dumps(res, sort_keys=False, indent=4, separators=(',', ': '), ensure_ascii=False,ignore_nan=True))
+	fp.close()
+	
 
 
 
