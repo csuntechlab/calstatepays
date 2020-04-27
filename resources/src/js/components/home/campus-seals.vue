a58380226eb107b2f7c2f61c54da19cdf0a13db6
carlos <carlos7legend@gmail.com>
2020-04-27 12:29:04 -0700

    add admission link to university seals

--- resources/src/js/components/home/campus-seals.vue
+++ resources/src/js/components/home/campus-seals.vue
@@ -11,7 +11,7 @@
                     </h2>
                 </div>
                 <div class="row no-gutters">
-                    <figure  v-for="(item, index) in universitySeals" :key="index" class="col-md col-sm-3 col-3">
+                    <figure  v-for="(item, index) in universitySeals" :key="index" class="col-md col-sm-3 col-3 homepageUniversities__svg" @click="handleUniversitySealClick(item.admission)">
                         <img :src="item.url" :alt="item.alt">
                         <figcaption class="text-center" >{{item.name}}</figcaption>
                     </figure>
@@ -30,15 +30,20 @@ export default {
             url: "",
             isShowing: false,
             universitySeals:[
-                {url: window.baseUrl + '/img/csuseals/long_beach_seal.svg',name:'Long Beach',alt:''},
-                {url: window.baseUrl + '/img/csuseals/poly_seal.svg',name:'Pomona',alt:''},
-                {url: window.baseUrl + '/img/csuseals/fullerton_seal.svg',name:'Fullerton',alt:''},
-                {url :window.baseUrl+ '/img/csuseals/northridge_seal.svg',name:'Northridge',alt:''},
-                {url: window.baseUrl + '/img/csuseals/dominguez_seal.svg',name:'Dominguez Hills',alt:''},
-                {url: window.baseUrl + '/img/csuseals/los_angeles_seal.svg',name:'Los Angeles',alt:''},
-                {url: window.baseUrl + '/img/csuseals/channel_islands_seal.svg',name:'Channel Islands',alt:''},                
+                {url: window.baseUrl + '/img/csuseals/long_beach_seal.svg',name:'Long Beach',alt:'',admission:'http://www.csulb.edu/admissions'},
+                {url: window.baseUrl + '/img/csuseals/poly_seal.svg',name:'Pomona',alt:'',admission:'https://www.cpp.edu/admissions/index.shtml'},
+                {url: window.baseUrl + '/img/csuseals/fullerton_seal.svg',name:'Fullerton',alt:'',admission:'http://admissions.fullerton.edu/'},
+                {url :window.baseUrl+ '/img/csuseals/northridge_seal.svg',name:'Northridge',alt:'',admission:'https://www.csun.edu/admissions-records'},
+                {url: window.baseUrl + '/img/csuseals/dominguez_seal.svg',name:'Dominguez Hills',alt:'',admission:'https://www.csudh.edu/records-registration/'},
+                {url: window.baseUrl + '/img/csuseals/los_angeles_seal.svg',name:'Los Angeles',alt:'',admission:'http://www.calstatela.edu/admissions'},
+                {url: window.baseUrl + '/img/csuseals/channel_islands_seal.svg',name:'Channel Islands',alt:'',admission:'https://www.csuci.edu/admissions/'},                
             ]
         };
+    },
+    methods: {
+        handleUniversitySealClick(admission){
+            window.open(admission, '_blank')
+        }
     }
 }
 </script>
